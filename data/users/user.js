1366b28f1224c78f32d849a7abdd0e60f2bbed53
Francisco Silva <silva18franciscosilva@gmail.com>
2024-10-02 21:28:10 +0100

    Autenticaçao Praticamente Acabada Falta Recuperar Password

--- data/users/user.js
+++ data/users/user.js
@@ -14,6 +14,7 @@ let RoleSchema = new Schema({
 });
 
 let userSchema = new Schema({
+  name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
