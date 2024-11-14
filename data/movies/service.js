09c8834b7c57e4aba54254029e3e31b4daae6b4c
ruibarbosa18 <rui.9c.n18@gmail.com>
2024-11-14 15:54:14 +0000

    aa

--- data/movies/service.js
+++ data/movies/service.js
@@ -145,12 +145,44 @@ function MovieService(movieModel) {
     try {
       const movie = await movieModel.findByIdAndDelete(id);
       if (!movie) {
-        throw new Error("Filme não encontrado");
+        throw new NotFoundError("Filme não encontrado");
       }
       return movie;
     } catch (err) {
       console.log(err);
-      throw(err);
+      throw err;
+    }
+  }
+
+  async function searchMovie(title, year, plot) {
+    // Verifica se o título do filme foi fornecido
+    if (!title) {
+      throw new ValidationError("Por favor, forneça o título do filme.");
+    }
+
+    try {
+      // Verifica se o filme já está no banco de dados
+      const existingMovie = await movieModel.findOne({ title: movie.title, year: movie.year });
+      if (existingMovie) {
+        console.log(
+          "Filme já existe no banco de dados, acedendo à base de dados..."
+        );
+        return existingMovie; // Se o filme já existe, retorna-o
+      }
+
+      // Chama o serviço que faz a requisição à OMDb API
+      const movie = await movieModel.getMovieByTitleYearAndPlot(
+        title,
+        year,
+        plot
+      );
+
+      // Se não existir, salva no banco de dados
+      const newMovie = await movieModel.create(movie);
+      return newMovie;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
   }
 
