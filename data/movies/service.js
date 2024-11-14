088b15a58c0f5ae208e7937fd2ed322ec82abc32
ruibarbosa18 <rui.9c.n18@gmail.com>
2024-11-14 16:09:49 +0000

    bug fix

--- data/movies/service.js
+++ data/movies/service.js
@@ -162,7 +162,7 @@ function MovieService(movieModel) {
 
     try {
       // Verifica se o filme já está no banco de dados
-      const existingMovie = await movieModel.findOne({ title: movie.title, year: movie.year });
+      const existingMovie = await movieModel.findOne({ title: movieModel.title, year: movieModel.year });
       if (existingMovie) {
         console.log(
           "Filme já existe no banco de dados, acedendo à base de dados..."
