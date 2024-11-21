15736313e7c791299ee58dba8749fc603a557949
ruibarbosa18 <rui.9c.n18@gmail.com>
2024-11-21 15:14:56 +0000

    aa

--- data/movies/movies.js
+++ data/movies/movies.js
@@ -9,6 +9,7 @@ const ratingSchema = new Schema({
 const commentsSchema = new Schema({
   user : { type: Schema.Types.ObjectId, ref: 'User' },
   comment : { type: String, required: true },
+  rating : { type: Number, required: true },
   createdAt : { type: Date, default: Date.now },
 });
 
