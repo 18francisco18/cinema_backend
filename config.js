1be852845fde9d641cbb373c3867d714fb8ba933
Francisco Silva <silva18franciscosilva@gmail.com>
2024-12-26 21:30:07 +0000

    Move MongoDB URI and secret key to environment variables

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  db: "mongodb+srv://18francisco18:P7ZepoGK0IyWd346@cinema.wj03m.mongodb.net/?retryWrites=true&w=majority&appName=Cinema",
-  secret: "supersecret",
+  db: process.env.MONGODB_URI || "mongodb+srv://18francisco18:P7ZepoGK0IyWd346@cinema.wj03m.mongodb.net/?retryWrites=true&w=majority&appName=Cinema",
+  secret: process.env.SECRET_KEY || "supersecret",
   expiresPassword: 1718531517,
   saltRounds: 10,
 };
